6481a6c296bada037d97023619c012f5e67455f4
Javier Bullrich <javier@bullrich.dev>
2019-11-24 21:10:38 +0100

    Catched flow in try/catch for error report

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,42 @@
 import * as github from '@actions/github';
+import * as core from '@actions/core';
 import { context } from '@actions/github/lib/github';
 import GithubApi from './Github/GithubApi';
 import PraceAction from './Prace';
 import { PullRequestData } from './PullRequestData';
 
+/**
+ * Execute the github action
+ */
 async function action() {
-	const githubToken = process.env.GITHUB_TOKEN!;
-	const octokit = new github.GitHub(githubToken);
+	try {
+		const githubToken = process.env.GITHUB_TOKEN!;
+		const octokit = new github.GitHub(githubToken);
 
-	// Have to cast to unknown to then cast to the correct type
-	const pullRequest: PullRequestData = (context.payload
-		.pull_request as unknown) as PullRequestData;
+		// Have to cast to unknown to then cast to the correct type
+		const pullRequest: PullRequestData = (context.payload
+			.pull_request as unknown) as PullRequestData;
 
-	if (!pullRequest) {
-		throw new Error(
-			"Payload doesn't contain `pull_request`. " +
+		if (!pullRequest) {
+			throw new Error(
+				'Payload doesn\'t contain `pull_request`. ' +
 				'Make sure this Action is being triggered by a pull_request event ' +
 				'(https://help.github.com/en/articles/events-that-trigger-workflows#pull-request-event-pull_request).'
-		);
+			);
+		}
+
+		const githubApi = new GithubApi(octokit);
+
+		const prace = new PraceAction(githubApi, pullRequest);
+		await prace.execute();
+	} catch (error) {
+		if (process.env.NODE_ENV === 'test') {
+			throw error;
+		}
+
+		core.error(error);
+		core.setFailed(error.message);
 	}
-
-	const githubApi = new GithubApi(octokit);
-
-	const prace = new PraceAction(githubApi, pullRequest);
-	await prace.execute();
 }
 
 action();
