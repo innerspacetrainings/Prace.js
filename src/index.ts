3fe4d8698ac94d49c3486207892da0b9ec67792f
Bullrich <javier.bullrich@innerspace.eu>
2019-12-09 13:19:48 +0100

    Added a log to get the object (find error)

--- src/index.ts
+++ src/index.ts
@@ -9,39 +9,39 @@ import { GithubApi } from './Github/GithubApi';
  * Execute the github action
  */
 async function action() {
-	try {
-		const githubToken = process.env.GITHUB_TOKEN!;
-		const octokit = new github.GitHub(githubToken);
+	const githubToken = process.env.GITHUB_TOKEN!;
+	const octokit = new github.GitHub(githubToken);
 
-		core.info('Starting Prace!');
+	core.info('Starting Prace!');
 
-		// Have to cast to unknown to then cast to the correct type
-		const pullRequest: PullRequestData = (context.payload
-			.pull_request as unknown) as PullRequestData;
+	// Have to cast to unknown to then cast to the correct type
+	const pullRequest: PullRequestData = (context.payload
+		.pull_request as unknown) as PullRequestData;
 
-		if (!pullRequest) {
-			throw new Error(
-				"Payload doesn't contain `pull_request`. " +
-					'Make sure you followed the instructions to configure your repository ' +
-					'(https://github.com/innerspacetrainings/Prace.js/blob/master/README.md).'
-			);
-		}
+	core.info(JSON.stringify(pullRequest));
 
-		const githubApi = new GithubApi(octokit);
-
-		const prace = new PraceAction(githubApi, pullRequest);
-		const result = await prace.execute();
-		core.info(
-			`Finished evaluating and found${result ? ' no ' : ' '}problems`
+	if (!pullRequest) {
+		throw new Error(
+			'Payload doesn\'t contain `pull_request`. ' +
+			'Make sure you followed the instructions to configure your repository ' +
+			'(https://github.com/innerspacetrainings/Prace.js/blob/master/README.md).'
 		);
-	} catch (error) {
-		if (process.env.NODE_ENV === 'test') {
-			throw error;
-		}
-
-		core.error(error);
-		core.setFailed(error.message);
 	}
+
+	const githubApi = new GithubApi(octokit);
+
+	const prace = new PraceAction(githubApi, pullRequest);
+	const result = await prace.execute();
+	core.info(
+		`Finished evaluating and found${result ? ' no ' : ' '}problems`
+	);
 }
 
-action();
+action().catch(error => {
+	if (process.env.NODE_ENV === 'test') {
+		throw error;
+	}
+
+	core.error(error);
+	core.setFailed(error.message);
+});
